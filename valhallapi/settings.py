444e5ec1cf9b180079c6b2622a5161ba3b07b2c0
QxEcR <qxecrx1110@gmail.com>
2022-03-17 08:00:39 +0300

    use postgresql

--- valhallapi/settings.py
+++ valhallapi/settings.py
@@ -71,9 +71,17 @@ WSGI_APPLICATION = 'valhallapi.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'itgkhvso',
+        'USER': 'itgkhvso',
+        'PASSWORD': 'rHu4Xq2Ks-SDnTBgI_HAVKEdarJiSxAP',
+        'HOST': 'kashin.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
